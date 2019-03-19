5c943f8e13aff953d52e5fec80c96246128c347c
ekmmrs <ekmmrs@gmail.com>
2019-03-18 17:51:59 -0700

    Revert "Adding demo and changes"
    This reverts commit 7e26edce472270081f05631acc1dbb14ed0c5bd8.

--- DMD_Demo.py
+++ DMD_Demo.py
@@ -8,12 +8,7 @@ Created on Thu Feb  7 16:25:16 2019
 import numpy as np
 from pydmd import DMD
 
-vals = np.array([[-2,6,1,1,-1],
-                 [-1,5,1,2,-1],
-                 [0,4,2,1,-1],
-                 [1,3,2,2,-1],
-                 [2,2,3,1,-1],
-                 [3,1,3,2,-1]])
+vals = np.array([[-2,6,1,1,-1],[-1,5,1,2,-1],[0,4,2,1,-1],[1,3,2,2,-1],[2,2,3,1,-1],[3,1,3,2,-1]])
 
 dmd = DMD(svd_rank = 2)
 vals_sub = vals[:5,:]
